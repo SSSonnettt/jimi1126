097d02c51f4d8f511904a24752ea4f9ab190a561
sonnet <xiefuqing_mid@163.com>
2026-05-31 00:44:50 +0800

    feat: add Cloudflare Pages deployment via OpenNext adapter

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+export default defineCloudflareConfig({
+  // No additional configuration needed for a static SSG blog
+});
